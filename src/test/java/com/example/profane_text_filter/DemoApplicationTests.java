e5e5ee8985700cc13624f4757fba2facdc3f888d
Anuj Kumar <anuj.kumar1@splashlearn.com>
2024-07-01 11:50:21 +0530

    first commit

---
+++ src/test/java/com/example/profane_text_filter/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profane_text_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
