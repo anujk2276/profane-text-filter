3ab20a6bca23308ced8ebbe0aa895228f654d530
Anuj Kumar <anuj.kumar1@splashlearn.com>
2024-07-05 12:11:47 +0530

    removed scheduler and added indexing

--- src/main/java/com/example/profaneTextFilter/Application.java
+++ src/main/java/com/example/profaneTextFilter/Application.java
@@ -2,13 +2,7 @@ package com.example.profaneTextFilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-//@EnableScheduling
-//@EnableJpaRepositories(basePackages = "com.example.profane_text_filter.repository")
-//@SpringBootApplication(scanBasePackages = "com.example.profaneTextFilter")
-//@EntityScan("com.example.profane_text_filter.model")
 @SpringBootApplication
 public class Application {
 
--- src/main/resources/db/migration/V202407021936__create_profane_words_table.sql
+++ src/main/resources/db/migration/V202407021936__create_profane_words_table.sql
@@ -5,3 +5,6 @@ CREATE TABLE profane_words (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- Add unique index to the 'word' column in the 'profane_words' table
+CREATE UNIQUE INDEX idx_word ON profane_words(word);
